5fef4b03451777f93074a75d9e064b8594280ecb
ArshS1 <90710790+ArshS1@users.noreply.github.com>
2023-04-13 14:55:25 -0400

    setup storage + function + backend

--- azure/src/functions/imaginize-fetcher.js
+++ azure/src/functions/imaginize-fetcher.js
@@ -1,13 +1,11 @@
 const { app } = require('@azure/functions');
 
 app.http('imaginize-fetcher', {
-    methods: ['GET', 'POST'],
+    methods: ['GET'],
     authLevel: 'anonymous',
     handler: async (request, context) => {
         context.log(`Http function processed request for url "${request.url}"`);
 
-        const name = request.query.get('name') || await request.text() || 'world';
-
-        return { body: `Hello, ${name}!` };
+        return { body: `Hello, world!` };
     }
 });
